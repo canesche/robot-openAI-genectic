9394985d69ad36af6ddde5904779f2a377f21fd3
canesche <michael.canesche@gmail.com>
2019-04-29 13:56:58 -0300

    estrategy if I lost, i want more

--- main.py
+++ main.py
@@ -7,99 +7,201 @@
 
 import gym
 import random
+import matplotlib.pyplot as plt
+import time
+import sys
+import numpy as np 
 
-# constants
-MAX_GENERATIONS = 30
-number_actions = 500
-percent_cross_over = 10 # e.g. 10 means 10% of total of actions
-percent_mutation = 0
+# constants global
+MAX_GENERATIONS = 100
+ACTIONS_SIZE = 500
+GENE_SIZE = 10
+POPULATION_SIZE = 100
+ELITE_SIZE = 20
+TOURNAMENT_SIZE = 10
+PERCENT_MUTATION = 20 # means 20% 
+EPSILON = 0.01
+DELTA = 0 
+env = gym.make("BipedalWalker-v2")
 
-def get_action(env):
-    return env.action_space.sample()
+def population_start():
+	ind = []
+	for _ in range(2*GENE_SIZE):
+		action = env.action_space.sample()
+		ind.append(action)
+	return ind
 
-def get_n_worst(rewards, qtd):
+def crossover(p1, p2):
 
-	vector = []
-	for i in range(len(rewards)):
-		vector.append((i, rewards[i]))
+	p1 = np.matrix(p1)
+	p2 = np.matrix(p2)
 
-	vector.sort(key=lambda tuple: tuple[1])
-	#print(vector)
+	part = random.randint(0, 4)
+
+	init_p1x = p1[:GENE_SIZE,0:part]
+	init_p2x = p2[:GENE_SIZE,part:4]
+
+	p1x = p1[GENE_SIZE:2*GENE_SIZE,0:part]
+	p2x = p2[GENE_SIZE:2*GENE_SIZE,part:4]
+
+	part = random.randint(0, 4)
+
+	init_p1y = p1[:GENE_SIZE,part:4]
+	init_p2y = p2[:GENE_SIZE,0:part]
+
+	p1y = p1[GENE_SIZE:2*GENE_SIZE,part:4]
+	p2y = p2[GENE_SIZE:2*GENE_SIZE,0:part]
+
+	aux_init_p1 = np.concatenate((init_p1x,init_p2x),axis=1)
+	aux_init_p2 = np.concatenate((init_p1y,init_p2y),axis=1)
+
+	aux_p1 = np.concatenate((p1x,p2x),axis=1)
+	aux_p2 = np.concatenate((p1y,p2y),axis=1)
+
+	c1 = list(np.array(aux_init_p1))+list(np.array(aux_p1))
+	c2 = list(np.array(aux_init_p2))+list(np.array(aux_p2))
+
+	return c1, c2
+
+def mutation(p):
+
+	for i in range(len(p)):
+		n = random.randint(0, 100)
+		#input(p[i])
+		if n <= PERCENT_MUTATION :
+			fator = EPSILON * (random.random() * 2-1)
+			p[i] = p[i] + fator
+			#print("passou",fator)
+		#input(p[i]) 
+		
+	return p
+
+def evaluate(p, render=False):
+	env.reset()
+	score = 0
+
+	#step begin, size 10 of p
+	for i in range(GENE_SIZE):
+		if render:
+			env.render()
+		_, reward, _, _ = env.step(p[i])
+		score += reward
 	
-	result = []
-	for i in range(qtd):
-		result.append(vector[i][0])
+	# loop until ACTION_SIZE
+	i = 0
+	for _ in range(GENE_SIZE, ACTIONS_SIZE):
+		if render:
+			env.render()
+		_, reward, _, _ = env.step(p[(i % GENE_SIZE)+10])
+		score += reward
+		i += 1
 
-	return result
-    
+	return score
+
+def tournament(p):
+	best_local = p[random.randint(0, POPULATION_SIZE-1)]
+
+	for _ in range(TOURNAMENT_SIZE):
+		n = random.randint(0, POPULATION_SIZE-1)
+		if best_local[0] < p[n][0]:
+			best_local = p[n].copy()
+	
+	return best_local
+
+def plot(value):
+	plt.plot(value)
+	plt.xlabel('Generations')
+	plt.ylabel('Adaptation value')
+	plt.savefig('adaptacao')
+
 # main function
 def main():
 
-	old_population = []
+	begin = time.time()
+	pop = []
+	for i in range(POPULATION_SIZE):
+		aux = population_start()
+		pop.append([evaluate(aux), aux])
+	time_spent = time.time()-begin
 
-	env = gym.make("BipedalWalker-v2")
+	best_global = pop[0]
 
-	for _ in range(number_actions):
-		old_population.append([-100, get_action(env)])
+	value_before = pop[0][0]
 
-	i = 0
-	old_total = -100
-	while i < MAX_GENERATIONS:
-		print("generation %i:" %i)
+	vector_fitness = []
+	for i in range(MAX_GENERATIONS):
 
-		env.reset()
+		begin = time.time()
+		pop.sort(reverse=True, key=lambda ind: ind[0])
 
-		arq = open("data/generation_"+str(i), "w")
+		best_individual = pop[0].copy()
 
-		new_population = old_population.copy()
-		new_total = 0.0
+		value_now = best_individual[0]
 
-		#print(new_population)
+		DELTA = value_now - value_before
+		value_before = value_now
 
-		# It`s not necessary to do crossover on the first generations
-		if i > 0 :
-			get_actions_change = get_n_worst(
-				[r[0] for r in new_population],
-				number_actions*percent_cross_over//100)
-			print(get_actions_change)
-			for k in get_actions_change :
-				new_population[k] = [-100, get_action(env)]
+		print("delta =",DELTA)
 
-		for j in range(number_actions):
+		# estrategia se perdi, eu quero eh mais
+		if DELTA > 0 :
+			PERCENT_MUTATION = 10
+			EPSILON = 0.001
+			ELITE_SIZE = 40
+		else :
+			PERCENT_MUTATION = 40
+			EPSILON = 0.02
+			ELITE_SIZE = 20
+
+		if best_individual[0] > best_global[0]:
+			best_global = best_individual.copy()
+
+		vector_fitness.append(best_individual[0])
+		
+		print("generation %i: %10.6lf time spent: %.2fs" 
+		%(i,best_individual[0], time_spent))
+
+		# fitness
+		if best_global[0] >= 100 or i == MAX_GENERATIONS-1:
 			
-			env.render()
+			print("melhor global =",best_global[0])
 
-			action = new_population[j][1]
-				
-			#print('Action: ', action)
-			observation, reward, done, info = env.step(action) # take a random action
+			evaluate(best_global[1], render=True)
 
-			new_population[j][0] = reward
-
-			arq.write(str(new_population[j][1])+"\n")
-			arq.write(str(new_population[j][0])+"\n")
-
-			if reward == -100 or j == number_actions-1 :
-				print(new_total)
-				if i == 0 :
-					old_total = new_total
-				elif new_total > old_total :
-					# update the new population
-					old_population = new_population.copy()
-					old_total = new_total
-					print("mudei o vetor old")
-				break
+			env.close()
 			
-			new_total += reward
+			plot(vector_fitness)
 			
-		arq.close()
+			break
 
-		i += 1
+		new_pop = []
+		for j in range(ELITE_SIZE):
+			aux = mutation(pop[j][1])
+			new_pop.append([evaluate(aux), aux])
 
+		generate_children = (POPULATION_SIZE - ELITE_SIZE) // 2
+
+		for j in range(generate_children):
+			par1 = tournament(pop)
+			par2 = tournament(pop)
+
+			child1, child2 = crossover(par1[1], par2[1])
+
+			eval1 = evaluate(child1)
+			eval2 = evaluate(child2)
+
+			new_pop.append([eval1,child1])
+			new_pop.append([eval2,child2])
+
+		pop = new_pop.copy()
+		time_spent = time.time()-begin
+	
 	env.close()
-
 
 if __name__ == "__main__":
 
+	if len(sys.argv) > 2 :
+		print(sys.argv[1])
+
     # Call main function
-    main()
+	main()
